bcde3a5c71c09e18e68bf0a737167c3489a7f991
Sergey Zakharchenko <szakharchenko@digital-loggers.com>
2019-02-01 07:18:18 +0000

    Adding support for GPIO descriptors
    Adding support for kernels 4.17 and above which use GPIO descriptors,
    not GPIO numbers since commit 9b00bc7b901ff672a9252002d3810fdf9489bc64
    "spi: spi-gpio: Rewrite to use GPIO descriptors" by linusw on Feb 12,
    2018.
    
    We properly allocate and clean up gpiod lookup tables from a fixed
    pool. Device IDs are created and freed dynamically as well to match
    to-be-created devices in gpiod lookup tables. Support for missing
    MOSI/MISO/CS lines is provided by stubbing the lookup table entries
    (so that lookups for further lines still pass).

--- spi-gpio-custom.c
+++ spi-gpio-custom.c
@@ -58,6 +58,12 @@
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_gpio.h>
 
+#include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+#include <linux/gpio/machine.h>
+#include <asm-generic/gpio.h>
+#endif
+
 #define DRV_NAME	"spi-gpio-custom"
 #define DRV_DESC	"Custom GPIO-based SPI driver"
 #define DRV_VERSION	"0.1"
@@ -98,6 +104,47 @@ module_param_array(bus3, uint, &bus_nump[3], 0);
 MODULE_PARM_DESC(bus3, "bus3" BUS_PARM_DESC);
 
 static struct platform_device *devices[BUS_COUNT_MAX];
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+
+#define BUS_NAME_MAX           32
+
+/* NB: BUS_SLAVE_COUNT_MAX #CS entries. */
+
+#define GPIOD_TABLE_TEMPLATE				                \
+    {							                \
+	.dev_id=NULL,					                \
+	    .table={					                \
+	    GPIO_LOOKUP(NULL, 0, "sck", GPIO_ACTIVE_HIGH),		\
+	    GPIO_LOOKUP(NULL, 0, NULL, GPIO_ACTIVE_HIGH),		\
+	    GPIO_LOOKUP(NULL, 0, NULL, GPIO_ACTIVE_HIGH),		\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 0,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 1,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 2,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 3,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 4,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 5,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 6,  GPIO_ACTIVE_HIGH),	\
+	    GPIO_LOOKUP_IDX(NULL, 0, NULL, 7,  GPIO_ACTIVE_HIGH),	\
+	    {}						                \
+	}						                \
+    }
+
+static struct gpiod_lookup_table gpiod_table_0=GPIOD_TABLE_TEMPLATE;
+static struct gpiod_lookup_table gpiod_table_1=GPIOD_TABLE_TEMPLATE;
+static struct gpiod_lookup_table gpiod_table_2=GPIOD_TABLE_TEMPLATE;
+static struct gpiod_lookup_table gpiod_table_3=GPIOD_TABLE_TEMPLATE;
+#undef GPIOD_TABLE_TEMPLATE
+
+static struct gpiod_lookup_table *gpiod_tables[BUS_COUNT_MAX]={
+	&gpiod_table_0,
+	&gpiod_table_1,
+	&gpiod_table_2,
+	&gpiod_table_3
+};
+
+#endif
+
 static unsigned int nr_devices;
 
 static void spi_gpio_custom_cleanup(void)
@@ -107,6 +154,13 @@ static void spi_gpio_custom_cleanup(void)
 	for (i = 0; i < nr_devices; i++)
 		if (devices[i])
 			platform_device_unregister(devices[i]);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+	for (i = 0; i < nr_devices; i++) {
+		gpiod_remove_lookup_table(gpiod_tables[i]);
+		kfree(gpiod_tables[i]->dev_id);
+	}
+#endif
 }
 
 static int __init spi_gpio_custom_get_slave_mode(unsigned int id,
@@ -202,6 +256,12 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 	struct spi_device *slave;
 	struct spi_board_info slave_info;
 	int mode, maxfreq, cs;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0)
+	struct gpiod_lookup_table *gpiod_table;
+	int cs_gpio;
+	struct gpio_chip *chip_sck, *chip_mosi, *chip_miso, *chip_cs;
+	char* dev_id;
+#endif
 
 
 	if (!bus_nump[id])
@@ -211,12 +271,90 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 	if (err)
 		goto err;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+	gpiod_table = gpiod_tables[id];
+
+	chip_sck = gpio_to_chip(params[BUS_PARAM_SCK]);
+	if (!chip_sck) {
+		printk(KERN_ERR PFX "nonexistent GPIO %d for bus%d SCK\n", params[BUS_PARAM_SCK], id);
+		err = -EINVAL;
+		goto err;
+	}
+	gpiod_table->table[0].chip_label = chip_sck->label;
+	gpiod_table->table[0].chip_hwnum = params[BUS_PARAM_SCK] - chip_sck->base;
+
+	if (params[BUS_PARAM_MOSI]) {
+		chip_mosi = gpio_to_chip(params[BUS_PARAM_MOSI]);
+		if (!chip_mosi) {
+			printk(KERN_ERR PFX "nonexistent GPIO %d for bus%d MOSI\n", params[BUS_PARAM_MOSI], id);
+			err = -EINVAL;
+			goto err;
+		}
+		gpiod_table->table[1].chip_label = chip_mosi->label;
+		gpiod_table->table[1].chip_hwnum = params[BUS_PARAM_MOSI] - chip_mosi->base;
+		gpiod_table->table[1].con_id = "mosi";
+	} else {
+		gpiod_table->table[1].chip_label = "mosi-stub-chip";
+		gpiod_table->table[1].con_id = "mosi-stub";
+	}
+
+	if (params[BUS_PARAM_MISO]) {
+		chip_miso = gpio_to_chip(params[BUS_PARAM_MISO]);
+		if (!chip_miso) {
+			printk(KERN_ERR PFX "nonexistent GPIO %d for bus%d MISO\n", params[BUS_PARAM_MISO], id);
+			err = -EINVAL;
+			goto err;
+		}
+		gpiod_table->table[2].chip_label = chip_miso->label;
+		gpiod_table->table[2].chip_hwnum = params[BUS_PARAM_MISO] - chip_miso->base;
+		gpiod_table->table[2].con_id = "miso";
+	} else {
+		gpiod_table->table[2].chip_label = "miso-stub-chip";
+		gpiod_table->table[2].con_id = "miso-stub";
+	}
+
+	for (i = 0; i < BUS_SLAVE_COUNT_MAX; i++) {
+		/* no more slaves? */
+		if (spi_gpio_custom_get_slave_mode(id, params, i) < 0)
+			break;
+
+		cs_gpio = spi_gpio_custom_get_slave_cs(id, params, i);
+		if (cs_gpio >= 0) {
+			chip_cs = gpio_to_chip(cs_gpio);
+			if (!chip_miso) {
+				printk(KERN_ERR PFX "nonexistent GPIO %d for bus%d #CS %d\n", cs_gpio, id, i);
+				err = -EINVAL;
+				goto err;
+			}
+			gpiod_table->table[3 + i].chip_label = chip_cs->label;
+			gpiod_table->table[3 + i].chip_hwnum = cs_gpio - chip_cs->base;
+			gpiod_table->table[3 + i].con_id = "cs";
+		} else {
+			gpiod_table->table[3 + i].chip_label = "cs-stub-chip";
+			gpiod_table->table[3 + i].con_id = "cs-stub";
+		}
+	}
+
+	dev_id = kmalloc(BUS_NAME_MAX+1, GFP_KERNEL);
+	if (snprintf(dev_id, BUS_NAME_MAX+1, "spi_gpio.%d", params[BUS_PARAM_ID]) >= BUS_NAME_MAX+1) {
+		printk(KERN_ERR PFX "bus id %d too large\n", id);
+		err = -EINVAL;
+		goto err_free;
+	}
+	gpiod_table->dev_id = dev_id;
+	gpiod_add_lookup_table(gpiod_table);
+#endif
+
 	/* Create BUS device node */
 
 	pdev = platform_device_alloc("spi_gpio", params[BUS_PARAM_ID]);
 	if (!pdev) {
 		err = -ENOMEM;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+		goto err_remove;
+#else
 		goto err;
+#endif
 	}
 
 	num_cs = 0;
@@ -236,6 +374,7 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 		num_cs = 1;
 	}
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 17, 0)
 	pdata.sck = params[BUS_PARAM_SCK];
 	pdata.mosi = gpio_is_valid(params[BUS_PARAM_MOSI])
 		? params[BUS_PARAM_MOSI]
@@ -243,6 +382,7 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 	pdata.miso = gpio_is_valid(params[BUS_PARAM_MISO])
 		? params[BUS_PARAM_MISO]
 		: SPI_GPIO_NO_MISO;
+#endif
 	pdata.num_chipselect = num_cs;
 
 	err = platform_device_add_data(pdev, &pdata, sizeof(pdata));
@@ -272,9 +412,11 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 
 		memset(&slave_info, 0, sizeof(slave_info));
 		strcpy(slave_info.modalias, "spidev");
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 17, 0)
 		slave_info.controller_data = (void *)((cs >= 0)
 			? cs
 			: SPI_GPIO_NO_CHIPSELECT);
+#endif
 		slave_info.max_speed_hz = maxfreq;
 		slave_info.bus_num = params[BUS_PARAM_ID];
 		slave_info.chip_select = i;
@@ -304,6 +446,15 @@ static int __init spi_gpio_custom_add_one(unsigned int id, unsigned int *params)
 
 err_unregister:
 	platform_device_unregister(pdev);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+err_remove:
+	gpiod_remove_lookup_table(gpiod_table);
+
+err_free:
+	kfree(dev_id);
+#endif
+
 err:
 	return err;
 }
